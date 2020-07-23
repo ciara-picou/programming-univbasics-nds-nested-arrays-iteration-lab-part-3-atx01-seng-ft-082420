c145cc4da5efc1e59e6c8517828e36cf94569722
Ciara Picou <ciarapicou@att.net>
2020-07-23 01:03:45 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,25 +1,4 @@
-# def join_nested_strings(src)
-#   # src will be an Array of Arrays of Strings and Integers
-#   # Combine all Strings present in the AoA into a single value and return it
-# end
 
-# def join_nested_strings(src)
-#   count = 0
-#   string = 
-  
-#   while count < src.length do
-#     inner_count = 0
-    
-#     while inner_count < src[count].length do
-#     if src[count][inner_count].is_a? String
-#       string = string + "" + src[count][inner_count]
-#       inner_count += 1
-#     end
-#     count += 1
-#   end
-# end
-# string
-# end
 
 def join_nested_strings(src)
   count = 0
@@ -29,12 +8,30 @@ def join_nested_strings(src)
     inner_count = 0
     
     while inner_count < src[count].length do
-    if src[count][inner_count].is_a? String
+    if src[count][inner_count].class == String
       string_array.push(src[count][inner_count])
+      end
       inner_count += 1
     end
     count += 1
-  end
 end
-string_array.join("")
+string_array.join(" ")
 end
+
+# def join_nested_strings(src)
+#   count = 0
+#   string_array = []
+  
+#   while count < src.length do
+#     inner_count = 0
+    
+#     while inner_count < src[count].length do
+#     if src[count][inner_count].is_a? String
+#       string_array.push(src[count][inner_count])
+#       end
+#       inner_count += 1
+#     end
+#     count += 1
+# end
+# string_array.join(" ")
+# end
